576de8d6b805227797ee06c10564572776511159
mendozanatalia <nmendoza@byupathway.edu>
2025-01-14 11:24:39 -0400

    home page first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// var currentYear = new Date().getFullYear();
+// document.getElementById('currentYear').innerHTML = currentYear;
+
+// var lastModified = new Date(document.lastModified);
+// document.getElementById('lastModified').innerHTML = lastModified;
