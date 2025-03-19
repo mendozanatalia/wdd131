af14b3b7e85f32fb79ef2af73f9d701ab4c89200
mendozanatalia <nmendoza@byupathway.edu>
2025-03-18 21:22:43 -0400

    temples second commit

---
+++ week02/scripts/temples.js
@@ -0,0 +1,9 @@
+// FOOTER
+// Get current year
+const currentYear = new Date().getFullYear();
+document.getElementById("current-year").textContent = currentYear;
+
+// Get last modified
+const lastModified = document.lastModified;
+document.getElementById("last-modified").textContent = lastModified;
+
