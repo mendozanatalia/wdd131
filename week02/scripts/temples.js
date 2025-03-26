ca28d199105ee94eeb56526e898d289ab3cca4d9
mendozanatalia <nmendoza@byupathway.edu>
2025-03-25 23:14:01 -0400

    hamburger first try

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,9 +1,24 @@
 // FOOTER
-// Get current year
+    // Get current year
 const currentYear = new Date().getFullYear();
 document.getElementById("current-year").textContent = currentYear;
 
-// Get last modified
+    // Get last modified
 const lastModified = document.lastModified;
 document.getElementById("last-modified").textContent = lastModified;
 
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+
+// HAMBURGER MENU
+// const hamburger = document.querySelector(".hamburger");
+// const navBar = document.querySelector(".nav-bar");
+
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle(".nav-bar");
+//     navBar.classList.toggle("active");
+// })
