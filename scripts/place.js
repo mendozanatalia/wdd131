6eefa34bf73dc168df6013b6c7fe65db8265e1d7
mendozanatalia <nmendoza@byupathway.edu>
2025-04-02 22:21:48 -0400

    getdate script added

--- scripts/place.js
+++
@@ -1,8 +0,0 @@
-// GET DATE
-    // Get current year
-const currentYear = new Date().getFullYear();
-document.getElementById("current-year").textContent = currentYear;
-
-    // Get last modified
-const lastModified = document.lastModified;
-document.getElementById("last-modified").textContent = lastModified;
