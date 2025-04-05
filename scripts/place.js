c1e2dfa5015e215d42a1bbe2eee5d78cd5cbfbf6
mendozanatalia <nmendoza@byupathway.edu>
2025-04-05 18:59:08 -0400

    windchill finished

--- scripts/place.js
+++ scripts/place.js
@@ -1,19 +1,19 @@
-let temperature = 8;
-let windSpeed = 10;
+const temperature = 8;
+const windSpeed = 10;
 
 function calculateWindChill(temp, speed) {
-    return
-    // I need a formula here
+    return 13.12 + 0.6215 * temp - 11.37 * speed ** 0.16 + 0.3965 * temp * speed ** 0.16;
 }
 
 function displayWindChill() {
     const element = document.getElementById("windchill");
+    let windchillValue = "N/A";
 
     if (temperature <= 10 && windSpeed > 4.8) {
+        windchillValue = `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+    }
 
-    ;
-
-    } else {
-        // If the conditions are not met, then display "N/A", which means "not applicable".
-    ;}
+    element.innerHTML = windchillValue;
 }
+
+displayWindChill();
