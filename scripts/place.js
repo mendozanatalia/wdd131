177de3a81b5fe4f9ebe6fa516af28930682dd672
mendozanatalia <nmendoza@byupathway.edu>
2025-04-05 17:50:50 -0400

    all html and css mostly done

---
+++ scripts/place.js
@@ -0,0 +1,19 @@
+let temperature = 8;
+let windSpeed = 10;
+
+function calculateWindChill(temp, speed) {
+    return
+    // I need a formula here
+}
+
+function displayWindChill() {
+    const element = document.getElementById("windchill");
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+
+    ;
+
+    } else {
+        // If the conditions are not met, then display "N/A", which means "not applicable".
+    ;}
+}
