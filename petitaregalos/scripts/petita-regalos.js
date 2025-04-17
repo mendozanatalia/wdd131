079238e8583e136fd2947606e223da045e024a68
mendozanatalia <nmendoza@byupathway.edu>
2025-04-17 12:31:32 -0400

    project base mainly done

---
+++ petitaregalos/scripts/petita-regalos.js
@@ -0,0 +1,6 @@
+// hamburger menu
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
